51882e8ffdcae7263ee2320c04f35879b96c8e24
Ryan Zec <code@ryanzec.com>
2014-06-21 05:29:49 -0400

    refactored collection

--- orm/collection.js
+++ orm/collection.js
@@ -2,7 +2,6 @@ var _ = require('lodash');
 var EventEmitter = require('events').EventEmitter;
 var hookable = require('./hookable');
 var bluebird = require('bluebird');
-var collection = Object.create(hookable);
 var buildFindObject = function(value) {
   var find = {};
 
@@ -16,114 +15,117 @@ var buildFindObject = function(value) {
 
   return find;
 };
+var collection = Object.create(hookable);
 
-collection.add = function(models) {
-  if(_.isFunction(models.toArray)) {
-    models = models.toArray();
-  }
-
-  if(!_.isArray(models)) {
-    models = [models];
-  }
-
-  this._data = this._data.concat(models);
-};
-
-collection.remove = function(values) {
-  if(this._data.length > 0) {
-    if(!_.isArray(values)) {
-      values = [values];
+_.extend(collection, {
+  add: function(models) {
+    if(_.isFunction(models.toArray)) {
+      models = models.toArray();
     }
 
-    values.forEach((function(value) {
-      var find = buildFindObject.apply(this, [value]);
+    if(!_.isArray(models)) {
+      models = [models];
+    }
 
-      var modelIndex = _.indexOf(this._data, _.find(this._data, find));
+    this._data = this._data.concat(models);
+  },
 
-      if(modelIndex !== -1) {
-        this._data.splice(modelIndex, modelIndex + 1);
+  remove: function(values) {
+    if(this._data.length > 0) {
+      if(!_.isArray(values)) {
+        values = [values];
       }
-    }).bind(this));
-  }
-};
 
-collection.getByIndex = function(index) {
-  if(this._data.length > 0) {
-    return this._data[index];
-  }
+      values.forEach((function(value) {
+        var find = buildFindObject.apply(this, [value]);
 
-  return undefined;
-};
+        var modelIndex = _.indexOf(this._data, _.find(this._data, find));
 
-collection.get = function(value) {
-  if(this._data.length > 0) {
-    var find = buildFindObject.apply(this, [value]);
-    var options = {
-      find: find
-    };
-
-    this.runHooks('beforeGetByPrimaryKey', [this, options]);
-
-    var model = _.find(this._data, options.find);
-
-    this.runHooks('afterGetByPrimaryKey', [this, model]);
-
-    return model;
-  }
-
-  return undefined;
-};
-
-collection.clear = function() {
-  this._data = [];
-};
-
-collection.toJSON = function(options) {
-  var json = null;
-
-  if(this._data.length > 0) {
-    json = [];
-
-    this._data.forEach(function(value, key) {
-      json.push(value.toJSON(options));
-    });
-  }
-
-  return json;
-};
-
-collection.toJSONWithRelationships = function() {
-  var defer = bluebird.defer();
-  var json = null;
-  var applyArguments = Array.prototype.slice.call(arguments, 0);
-  var callsLeft = this._data.length;
-
-  if(this._data.length > 0) {
-    json = [];
-
-    this._data.forEach(function(value, key) {
-      value.toJSONWithRelationships.apply(value, applyArguments).then(function(results) {
-        json.push(results);
-
-        callsLeft -= 1;
-
-        if(callsLeft === 0) {
-          defer.resolve(json);
+        if(modelIndex !== -1) {
+          this._data.splice(modelIndex, modelIndex + 1);
         }
-      }, function(error) {
-        defer.reject(error);
+      }).bind(this));
+    }
+  },
+
+  getByIndex: function(index) {
+    if(this._data.length > 0) {
+      return this._data[index];
+    }
+
+    return undefined;
+  },
+
+  get: function(value) {
+    if(this._data.length > 0) {
+      var find = buildFindObject.apply(this, [value]);
+      var options = {
+        find: find
+      };
+
+      this.runHooks('beforeGetByPrimaryKey', [this, options]);
+
+      var model = _.find(this._data, options.find);
+
+      this.runHooks('afterGetByPrimaryKey', [this, model]);
+
+      return model;
+    }
+
+    return undefined;
+  },
+
+  clear: function() {
+    this._data = [];
+  },
+
+  toJSON: function(options) {
+    var json = null;
+
+    if(this._data.length > 0) {
+      json = [];
+
+      this._data.forEach(function(value, key) {
+        json.push(value.toJSON(options));
       });
-    });
-  } else {
-    defer.resolve(json);
+    }
+
+    return json;
+  },
+
+  toJSONWithRelationships: function() {
+    var defer = bluebird.defer();
+    var json = null;
+    var applyArguments = Array.prototype.slice.call(arguments, 0);
+    var callsLeft = this._data.length;
+
+    if(this._data.length > 0) {
+      json = [];
+
+      this._data.forEach(function(value, key) {
+        value.toJSONWithRelationships.apply(value, applyArguments).then(function(results) {
+          json.push(results);
+
+          callsLeft -= 1;
+
+          if(callsLeft === 0) {
+            defer.resolve(json);
+          }
+        }, function(error) {
+          defer.reject(error);
+        });
+      });
+    } else {
+      defer.resolve(json);
+    }
+
+    return defer.promise;
+  },
+
+  toArray: function() {
+    return this._data;
   }
-
-  return defer.promise;
-};
-
-collection.toArray = function() {
-  return this._data;
-};
+});
 
 Object.defineProperties(collection, {
   length: {
